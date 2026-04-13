b10ba6ba69d13b9117c49f0221fb524b5a9b4a74
Okolie Samuel <okechukwu.okolie.2023@gmail.com>
2026-04-13 15:18:10 +0100

    first commit

--- exer2/COS203-exer.py
+++ exer2/COS203-exer.py
@@ -11,3 +11,18 @@ print('')
 sqrt_144 = math.sqrt(144)
 print("Square root of 144:", sqrt_144)
 print('')
+
+#3. Calculate the sine, cosine, and tangent of 30 degrees.
+sine_30 = math.sin(math.radians(30))
+cosine_30 = math.cos(math.radians(30))
+tangent_30 = math.tan(math.radians(30))
+print("Sine of 30 degrees:", sine_30)
+print('')
+print("Cosine of 30 degrees:", cosine_30)
+print('')
+print("Tangent of 30 degrees:", tangent_30)
+print('')
+
+#4.Compute the natural logarithm of 50
+log_50 = math.log(50)
+print("Natural logarithm of 50:", log_50)
